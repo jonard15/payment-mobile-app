41841980314d369e433584dba585249f48cfeecc
Jonard siena <sienajonard15@gmail.com>
2022-05-26 11:53:54 +0800

    qr

--- src/views/QrView.vue
+++ src/views/QrView.vue
@@ -1,5 +1,11 @@
 <template>
     <div>
+    <v-toolbar flat>
+      <v-toolbar-title class="flex text-center text-h4">
+        <span>QR Reader</span>
+      </v-toolbar-title>
+    </v-toolbar>
+    <v-divider class="mb-5"></v-divider>
         <qrcode-stream @decode="onDecode"></qrcode-stream>
         <!-- <qrcode-drop-zone></qrcode-drop-zone> -->
         <!-- <qrcode-capture></qrcode-capture> -->
