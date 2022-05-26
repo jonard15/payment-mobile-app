77536635f79ea40cf5544b1d691f290a55be2adb
Jonard siena <sienajonard15@gmail.com>
2022-05-26 11:36:47 +0800

    test qr

--- src/views/QrView.vue
+++ src/views/QrView.vue
@@ -1,7 +1,16 @@
 <template>
     <div>
-        <qrcode-stream></qrcode-stream>
-        <qrcode-drop-zone></qrcode-drop-zone>
+        <qrcode-stream @decode="onDecode"></qrcode-stream>
+        <!-- <qrcode-drop-zone></qrcode-drop-zone> -->
         <!-- <qrcode-capture></qrcode-capture> -->
     </div>
 </template>
+<script>
+export default {
+    methods: {
+        onDecode (decodedString) {
+        alert(decodedString)
+        }
+    }
+}
+</script>
